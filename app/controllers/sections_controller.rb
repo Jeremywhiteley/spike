3c0ac470136956065bf46e06f73b715c8469bd56
toby cabot <ccabot@mitre.org>
2012-12-12 16:03:38 -0500

    remove explicit respond_do statements in section controller
    Looks like they're not needed - rails will DTRT without them.  BTW, I
    know I've waffled on taking them out and putting them back.  Hopefully
    I won't need to change my mind again, but you never know.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -5,19 +5,9 @@ class SectionsController < ApplicationController
   end
 
   def medication
-    respond_to do |format|
-      format.html
-      format.atom
-      format.xml
-    end
   end
 
   def demographic
-    respond_to do |format|
-      format.html
-      format.atom
-      format.xml
-    end
   end
 
   def find_person
