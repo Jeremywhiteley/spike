71b620e6917f79b5c0fbe8ebdb15d57eafbf144f
toby cabot <toby@caboteria.org>
2012-12-17 23:30:55 -0500

    show address in the people index listing

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ActiveRecord::Base
   attr_accessible :city, :postal_code, :state, :street
+
+  def to_s
+    s = ""
+    s += street + ", " if street.present?
+    s += city + ", " if city.present?
+    s += state + " " if state.present?
+    s += postal_code if postal_code.present?
+    s
+  end
+
 end
