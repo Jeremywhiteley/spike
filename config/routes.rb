8e20131c6beabbc1b1d07ae43606122e4059c378
toby cabot <toby@caboteria.org>
2013-01-05 10:15:46 -0500

    refactor routing rules
    I've heard that "match" will be deprecated in favor of "get", "put",
    etc.  Also removed people option route which was unimplemented.

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ PdmpSpike::Application.routes.draw do
 
   devise_for :users
 
-  match "people/:id/root.xml" => "people#root", :as => :root_document, :format => :xml, :via => :get
-  match "people/:id" => "people#options", :as => :root_options, :via => :options
+  get "people/:id/root.xml" => "people#root", :as => :root_document, :format => :xml
 
-  match "people/:id/c32" => "sections#c32", as: "person_c32_section", via: :get
-  match "people/:id/c32/c32" => "sections#c32", as: "person_c32_doc", via: :get
-  match "people/:id/demographic" => "sections#demographic", as: "person_demographic_section", via: :get
-  match "people/:id/demographic/demographic" => "sections#demographic", as: "person_demographic_doc", via: :get
-  match "people/:id/medication" => "sections#medication", as: "person_medication_section", via: :get
+  get "people/:id/c32" => "sections#c32", as: "person_c32_section"
+  get "people/:id/c32/c32" => "sections#c32", as: "person_c32_doc"
+  get "people/:id/demographic" => "sections#demographic", as: "person_demographic_section"
+  get "people/:id/demographic/demographic" => "sections#demographic", as: "person_demographic_doc"
+  get "people/:id/medication" => "sections#medication", as: "person_medication_section"
 
   resources :people
   resources :prescriptions
