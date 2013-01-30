ede93385e27196f438737db58e46c4255a04d0a2
toby cabot <toby@caboteria.org>
2013-01-30 15:10:47 -0500

    Revert "add headers to allow cross-origin sharing"
    This reverts commit acae7c7e47fc9d7055a98c1f913ce89fd099c3f8.  We
    needed these headers to help debug the initial integration but they're
    no longer useful.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   before_filter :authenticate_user!
-  after_filter :add_access_control_headers
   layout Proc.new { |c| c.user_signed_in? ? "auth" : "unauth" }
-
-  private
-
-  def add_access_control_headers
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Request-Method"] = "*"
-  end
-
 end
