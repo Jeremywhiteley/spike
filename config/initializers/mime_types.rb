81d497220e7d28f27555c9e49bd2b0ab7c079a4d
toby cabot <ccabot@mitre.org>
2013-03-15 10:52:40 -0400

    add a MIME type for NCPDP script xml documents

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -4,3 +4,4 @@
 Mime::Atom = Mime::Type.lookup_by_extension(:atom)
 Mime::Type.register "application/opensearchdescription+xml", :opensearchdescription
 Mime::Type.register "application/vnd.mitre.pdmp.2+xml", :pdmp2
+Mime::Type.register "application/vnd.ncpdp.script.10+xml", :ncpdpscript
